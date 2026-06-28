61c916849cdc2a83791ec58eebc7275926fd3ba2
asoumare <asoumare@f4r4s3.paris.42.school>
2026-06-29 01:48:18 +0200

    fin exo 2

--- level2/ft_otp.py
+++ level2/ft_otp.py
@@ -1,53 +1,63 @@
 import sys
-
+import time
+import hmac
+import hashlib
+import struct
+from pathlib import Path
 
 def validate_and_save_key(filename):
-    """Gère l'option -g : vérifie la clé d'un fichier et la sauvegarde."""
     try:
-        # Lecture du fichier contenant la clé
         with open(filename, 'r') as f:
             cle_hex = f.read().strip()
     except FileNotFoundError:
         print(f"Error: File '{filename}' not found.")
         return
 
-    # 1. Vérification de la taille (exactement 64 caractères)
     if len(cle_hex) != 64:
         print("./ft_otp: error: key must be 64 hexadecimal characters.")
         return 
     
-    # 2. Vérification de l'hexadécimal
     for char in cle_hex:
         if (char < '0' or char > '9') and (char < 'a' or char > 'f') and (char < 'A' or char > 'F'):
             print("./ft_otp: error: key must be 64 hexadecimal characters.")
             return
     
-    # 3. Conversion et "chiffrement" (On transforme l'hexa en octets bruts)
     try:
         key_bytes = bytes.fromhex(cle_hex)
     except ValueError:
         print("./ft_otp: error: key must be 64 hexadecimal characters.")
         return
 
-    # Sauvegarde dans ft_otp.key
-    with open("ft_otp.key", "wb") as f:
+    new_file = str(Path(filename).with_suffix(".key"))
+    with open(new_file, "wb") as f:
         f.write(key_bytes)
     
-    print("Key was successfully saved in ft_otp.key.")
+    print("Key was successfully saved in " + filename)
 
 
 def ft_otp(filename):
-    """Gère l'option -k : lit la clé et génère le mot de passe à usage unique."""
     try:
-        # Lecture de la clé en octets bruts
         with open(filename, 'rb') as f:
             key_bytes = f.read()
     except FileNotFoundError:
         print(f"Error: File '{filename}' not found.")
         return
 
+    current_time = int(time.time())
+    counter = current_time // 30
+
+    counter_bytes = struct.pack(">Q", counter)
+
+    mac = hmac.new(key_bytes, counter_bytes, hashlib.sha1).digest()
+
+    offset = mac[-1] & 0x0f
+    binary = struct.unpack('>I', mac[offset:offset+4])[0] & 0x7fffffff
+    
+    otp = binary % 1000000
+    
+    print(f"{otp:06d}")
+
 def main():
-    # Vérification du nombre d'arguments
     if len(sys.argv) != 3:
         print("Usage:\n  ./ft_otp -g <key_file.hex>\n  ./ft_otp -k <ft_otp.key>")
         sys.exit(1)
@@ -55,7 +65,6 @@ def main():
     flag = sys.argv[1]
     filename = sys.argv[2]
 
-    # Routage selon l'argument utilisé
     if flag == "-g":
         validate_and_save_key(filename)
     elif flag == "-k":
@@ -65,3 +74,10 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+# uint32_t binary = ((mac[offset] & 0x7f) << 24) |
+#                   (mac[offset + 1] << 16) |
+#                   (mac[offset + 2] << 8)  |
+#                   (mac[offset + 3]);
